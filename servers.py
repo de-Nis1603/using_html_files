cd057ed2f151e4a6c9506eb7dfedf22fa004eefb
Den <no@>
2024-03-22 17:12:21 +0300

    Список профессий

--- servers.py
+++ servers.py
@@ -11,5 +11,9 @@ def index(site_name):
 def find_job(prof):
     return render_template('base.html', job=prof)
 
+@app.route('/list_prof/<param>')
+def print_list(param):
+    return render_template('prof_list.html', p=param)
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
