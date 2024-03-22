5dd18e725d727fae49c6bc4ccbaa7a01b1ca86b4
Den <no@>
2024-03-22 16:41:33 +0300

    Тренировки в полете

--- servers.py
+++ servers.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 
@@ -7,6 +6,10 @@ app = Flask(__name__)
 @app.route('/index/<site_name>')
 def index(site_name):
     return render_template('index.html', title=site_name)
+
+@app.route('/training/<prof>')
+def find_job(prof):
+    return render_template('base.html', job=prof)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
