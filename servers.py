c8350ca89323175818635fb41ee7f3f7a53d8bd6
Den <no@>
2024-04-03 16:37:19 +0300

    Двойная защита

--- servers.py
+++ servers.py
@@ -1,6 +1,17 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+class LoginForm(FlaskForm):
+    username_crew = StringField('id астронавта', validators=[DataRequired()])
+    password_crew = PasswordField('Пароль астронавта', validators=[DataRequired()])
+    username_cap = StringField('id капитана', validators=[DataRequired()])
+    password_cap = PasswordField('пароль капитана', validators=[DataRequired()])
+    submit = SubmitField('Войти')
 
 @app.route('/')
 @app.route('/index/<site_name>')
@@ -9,11 +20,22 @@ def index(site_name):
 
 @app.route('/training/<prof>')
 def find_job(prof):
-    return render_template('base.html', job=prof)
+    return render_template('training.html', job=prof)
 
 @app.route('/list_prof/<param>')
 def print_list(param):
     return render_template('prof_list.html', p=param)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_form_creator():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('emergency_login.html', title='Авторизация', form=form)
+
+@app.route('/success')
+def successful_login():
+    return 'success'
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
